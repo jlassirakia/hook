08bc86df9db3f24ab22a94b1ed1ae0a347139482
jlassirakia <jlassirakia1919@gmail.com>
2021-04-27 22:01:39 +0100

    first commit

---
+++ src/Components/Data.js
@@ -0,0 +1,51 @@
+export const Data = [
+    {
+      id: Math.random(),
+      image: 'https://fr.web.img5.acsta.net/pictures/21/04/22/15/18/0089345.jpg',
+      rating: 2,
+      name: 'Conjuring3',
+      date: '2021',
+      description:
+        'Le très attendu film dhorreur porté par Vera Farmiga et Patrick Wilson doit sortir au mois de juin dans nos salles. ',
+    },
+    {
+      id: Math.random(),
+      image: 'https://sep.yimg.com/ca/I/yhst-55466754130796_2272_278782832',
+      rating: 4,
+      name: 'المتسول',
+      date: '2002',
+      description:
+        'film égyptien adel imam',
+    },
+   
+    {
+      id: Math.random(),
+      image: 'http://culturaddict.com/wp-content/uploads/2016/09/exorcist-art-list-christopher-franchi.jpg',
+      rating: 2,
+      name: 'lexorciste',
+      date: '1973',
+      description:
+        "L'Exorciste (The Exorcist) est un film d'horreur américain réalisé par William Friedkin, sorti en 1973 ",
+    },
+    {
+      id: Math.random(),
+      image:
+        'http://www.babunga.alobi.cd/wp-content/uploads/2017/12/movieposter-279x400.jpg',
+      rating: 5,
+      name: 'Titanic',
+      date: '1997',
+      description:
+        "Titanic est un film dramatique américain écrit, produit et réalisé par James Cameron, sorti en 1997",
+    },
+  
+    {
+      id: Math.random(),
+      image:
+        'https://cdn1.webmanagercenter.com/tekiano/wp-content/uploads/2019/01/dachra-affiche-1.jpg',
+      rating: 5,
+      name: 'Dachra',
+      date: '2019',
+      description:
+        "Dachra est un film d'horreur tunisien écrit et réalisé par Abdelhamid Bouchnak et sorti en 2018. Il est le premier film tunisien rentable en vingt ans",
+    },
+  ]
